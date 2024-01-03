315bfb90e0726d219347f724aaf1d369a796a5e3
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-03 22:48:52 +0300

    Cloned

---
+++ src/test/java/com/workintech/basicsecurity/BasicSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.basicsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
