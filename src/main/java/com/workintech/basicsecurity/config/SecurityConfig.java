cbe852204e5f5413fe8d38ccb7bf5285e490e6f2
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-03 23:57:17 +0300

    Security filter chain

--- src/main/java/com/workintech/basicsecurity/config/SecurityConfig.java
+++ src/main/java/com/workintech/basicsecurity/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.workintech.basicsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -22,5 +26,21 @@ public class SecurityConfig {
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return new ProviderManager(daoAuthenticationProvider);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/auth/**").permitAll();
+                    auth.requestMatchers(HttpMethod.GET, "/accounts/**").hasAnyAuthority("ADMIN", "USER");
+                    auth.requestMatchers(HttpMethod.POST, "/accounts/**").hasAuthority("ADMIN");
+                    auth.requestMatchers(HttpMethod.PUT, "/accounts/**").hasAuthority("ADMIN");
+                    auth.requestMatchers(HttpMethod.DELETE, "/accounts/**").hasAuthority("ADMIN");
+                    auth.anyRequest().authenticated();
+                })
+                //.formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
