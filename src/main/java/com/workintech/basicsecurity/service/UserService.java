9fd8edd3247c671fc7b8547ad2e08a2128dbcf91
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-03 23:19:48 +0300

    UserService implements UserDetailsService

---
+++ src/main/java/com/workintech/basicsecurity/service/UserService.java
@@ -0,0 +1,21 @@
+package com.workintech.basicsecurity.service;
+
+import com.workintech.basicsecurity.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("Email is not valid")
+        );
+    }
+}
