a7ca91ebb9eeef4890eb2611632c3042719c2b45
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-03 23:30:22 +0300

    register method: member object created and saved

--- src/main/java/com/workintech/basicsecurity/service/AuthenticationService.java
+++ src/main/java/com/workintech/basicsecurity/service/AuthenticationService.java
@@ -1,13 +1,16 @@
 package com.workintech.basicsecurity.service;
 
 import com.workintech.basicsecurity.entity.user.Member;
+import com.workintech.basicsecurity.entity.user.Role;
 import com.workintech.basicsecurity.repository.MemberRepository;
 import com.workintech.basicsecurity.repository.RoleRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @AllArgsConstructor
 @Service
@@ -21,5 +24,17 @@ public class AuthenticationService {
         if (foundMember.isPresent()) throw new RuntimeException("User with the following email already exists: " + email);
 
         String encodedPassword = passwordEncoder.encode(password);
+
+        Role userRole = roleRepository.findByAuthority("USER").get();
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(userRole);
+
+        Member member = new Member();
+        member.setEmail(email);
+        member.setPassword(encodedPassword);
+        member.setAuthorities(roles);
+
+        return memberRepository.save(member);
     }
 }
